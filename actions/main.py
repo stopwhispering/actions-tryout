57b66ca59316d63b157f520e44a57da9c624e650
Johannes <stopwhispering314@gmail.com>
2023-02-24 11:26:08 +0100

    add pylint

---
+++ actions/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    if __name__ == "__main__":
+        uvicorn.run(
+            "actions.main:app",
+            host="localhost",
+            port=5000,
+            # reload=True
+        )
